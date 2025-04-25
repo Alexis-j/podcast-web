9cbb27391219eaca6d2eb51914c793743c57f644
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2025-04-25 14:56:15 +0200

    integrantes page estructura

--- src/components/integranteCard/IntegranteCard.jsx
+++ src/components/integranteCard/IntegranteCard.jsx
@@ -4,25 +4,25 @@ import React from 'react';
 import { getImageUrl } from '../../utils/getImageUrl';
 
 const IntegranteCard = ({ nombre, bio, foto_url, ep_1_url, ep_2_url, ep_3_url }) => {
-  // Imprimir los datos de las props para ver qué estamos recibiendo
-  console.log('Integrante Card Data:', { nombre, bio, foto_url, ep_1_url, ep_2_url, ep_3_url });
   const finalUrl = getImageUrl(foto_url.trim());
-console.log('📸 URL Final:', finalUrl);
-
 
   return (
-    <div className="integrante-card">
-      {/* Mostrar la foto usando la función getImageUrl */}
-      <img src={getImageUrl(foto_url.trim())} alt={nombre} />
+    <section>
+      <div className="integrante-card">
+        <img src={getImageUrl(foto_url.trim())} alt={nombre} />
+        <div className="card-info">
+          <h2>{nombre}</h2>
+          <p>{bio}</p>
 
-      <h2>{nombre}</h2>
-      <p>{bio}</p>
-      <div className="episodios">
-        {ep_1_url && <a href={ep_1_url} target="_blank" rel="noopener noreferrer">🎧 Episodio 1</a>}
-        {ep_2_url && <a href={ep_2_url} target="_blank" rel="noopener noreferrer">🎧 Episodio 2</a>}
-        {ep_3_url && <a href={ep_3_url} target="_blank" rel="noopener noreferrer">🎧 Episodio 3</a>}
+          <h3>fav armas</h3>
+          <div className="episodios">
+            {ep_1_url && <a href={ep_1_url} target="_blank" rel="noopener noreferrer">sierra</a>}
+            {ep_2_url && <a href={ep_2_url} target="_blank" rel="noopener noreferrer">cuchillo</a>}
+            {ep_3_url && <a href={ep_3_url} target="_blank" rel="noopener noreferrer">maso</a>}
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
--- src/utils/getImageUrl.jsx
+++ src/utils/getImageUrl.jsx
@@ -5,6 +5,5 @@ export const getImageUrl = (filename) => {
   const cleanFilename = filename.trim(); // elimina espacios, saltos de línea, etc.
   const path = `${cleanFilename}`;
   const { data } = supabase.storage.from('integrantes').getPublicUrl(path);
-  console.log("📸 URL Final:", data?.publicUrl); // para verificar
   return data?.publicUrl || '';
 };
